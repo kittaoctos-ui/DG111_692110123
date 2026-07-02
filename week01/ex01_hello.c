45ae25ebb96601718a4aa368dfac554f1a1067fc
Krittapas Yana <krittapasyana2556@gmail.com>
2026-07-02 15:39:47 +0700

    Add flowcharts for grade evaluation, maximum of two numbers, and counting to N
    - Created flowchart for grade evaluation based on score thresholds (A, B, C, D, F).
    - Added flowchart for determining the maximum of two input numbers.
    - Implemented flowchart for counting from 1 to N.
    - Introduced combat and level-up systems flowcharts for game mechanics.

---
+++ week01/ex01_hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("สวัสดีคับ ผม กฤตภาส ยานะ\n");
+    printf("รหัสนักศึกษา : 692110123\n");
+    printf("วิชา : DG111 Computer  Programing");
+    return 0;
+}
